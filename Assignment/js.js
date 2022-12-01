6aa7a4c797de2de1482f262f4ab904b2b0e4082c
trongtien0807 <nguyenhotrongtien0807@gmail.com>
2022-12-01 20:15:03 +0700

    HTML_CSS_JS_Bootstrap_web_tin_tuc

---
+++ Assignment/js.js
@@ -0,0 +1,11 @@
+// ----------------- Sticky navbar ---------------
+
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("navbar").style.top = "0px";
+//     } 
+//     else {
+//     document.getElementById("navbar").style.top = "100px";
+//     }
+// }
